6964f675577351a535e6990f1fcd61854f38d6e8
gustavocarvalho96-lang <gustavocarvalho96@hotmail.com.br>
2026-05-15 10:07:41 -0300

    updt

---
+++ shared/sensitive-data.js
@@ -0,0 +1,28 @@
+window.GAPPS_SENSITIVE_DATA = {
+  // Senha temporaria: 1234
+  // Para trocar, substitua pelo SHA-256 hexadecimal da nova senha.
+  passwordHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
+  title: "Dados sensiveis",
+  sections: [
+    {
+      title: "Produtividade",
+      lines: [
+        "17560",
+        "190584rO@"
+      ]
+    },
+    {
+      title: "Power BI",
+      lines: [
+        "administrativo@hcsvicente.onmicrosoft.com",
+        "Senha: @hsv%Adm##",
+        "",
+        "pas.gestao.pbi@hsvicente.org.br",
+        "Senha: Pagest@Pbi100",
+        "",
+        "pas.pbi@hsvicente.org.br",
+        "Senha: PronAt@Pbi15"
+      ]
+    }
+  ]
+};
